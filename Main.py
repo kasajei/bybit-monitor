130780a0dbdf81140da4ce679a8fddf37642d459
kasajei <sevenbluesea.yk@gmail.com>
2024-01-04 19:08:23 +0900

    fixd: devide 0

--- Main.py
+++ Main.py
@@ -48,7 +48,7 @@ def fetch_wallet(account_name, account, datetime):
         'wallet_balance': bybit.wallet.wallet_balance,
         'cum_realised_pnl': bybit.wallet.cum_realised_pnl,
         'unrealised_pnl': bybit.wallet.unrealised_pnl,
-        'unrealised_%': round(float(bybit.wallet.unrealised_pnl)/ float(bybit.wallet.wallet_balance) * 100, 2) if bybit.wallet.unrealised_pnl != 0 else 0,
+        'unrealised_%': round(float(bybit.wallet.unrealised_pnl)/ float(bybit.wallet.wallet_balance) * 100, 2) if bybit.wallet.wallet_balance != 0 else 0,
         'time': bybit.wallet.time,
     }
 
